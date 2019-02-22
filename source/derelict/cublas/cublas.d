21fe644844ec6f1319a1c4f6fa6b6642bda98666
naetherm <naether.markus@gmail.com>
2019-02-22 18:47:25 +0100

    Small fixes.

--- source/derelict/cublas/cublas.d
+++ source/derelict/cublas/cublas.d
@@ -1,5 +1,8 @@
+// Copyright 2019, University of Freiburg.
+// Chair of Algorithms and Data Structures.
+// Markus Näther <naetherm@informatik.uni-freiburg.de>
 
-module derelict.cublas;
+module derelict.cublas.cublas;
 
 import derelict.util.loader;
 import derelict.cuda.runtimeapi;
